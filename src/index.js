ea41eb137fa79fb2eb74123763b895c29a363b5c
Christian Stevens <stev1905@gmail.com>
2019-12-24 00:01:12 -0500

    broken homepage into smaller components, finished styling our menu-item component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './homepage.component'
+import './pages/homepage/homepage.component';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
