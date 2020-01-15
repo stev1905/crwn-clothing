abeaebacd061d20e37ee0ec575d03824a7fcbcc3
Christian Stevens <cstevens@911MEMORIAL.ORG>
2020-01-15 16:29:07 -0500

    added redux-persist functionality to cache cart state

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -12,7 +12,7 @@ const CheckoutItem = ( {cartItem, clearItem, addItem, removeItem} ) => {
             <img src={imageUrl} alt='item' />
         </div>
         <span className='name'>{name}</span>
-        <span className='quantity'>,
+        <span className='quantity'>
         <div className='arrow' onClick={()=> removeItem(cartItem)}>&#10094;</div>
             <span className='value'>{quantity}</span>
         <div className='arrow' onClick={()=> addItem(cartItem)}>&#10095;</div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import {createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
-import combineReducers from './root-reducer';
+import rootReducers from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(combineReducers, applyMiddleware(...middlewares));
+export const store = createStore(rootReducers, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
