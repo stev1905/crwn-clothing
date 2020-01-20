eea517c14d95113b9c94060319988b775becf9d3
Christian Stevens <stev1905@gmail.com>
2020-01-19 23:13:55 -0500

    removed addCollectionAndDocument function from App.js since shop collections and docs have been added already to firestore

--- src/App.js
+++ src/App.js
@@ -11,16 +11,15 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component.jsx';
-import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
-import { selectCollectionForPreview } from './redux/shop/shop.selector'; 
 
 class App extends Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const { setCurrentUser, collectionsArray } = this.props;
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
@@ -34,7 +33,6 @@ class App extends Component {
       }
 
       setCurrentUser(userAuth);
-      addCollectionAndDocuments('collections', collectionsArray.map(({title, items}) => ({ title, items }))) 
     })
   }
   componentWillUnmount(props) {
@@ -68,7 +66,6 @@ class App extends Component {
 
 const mapStateToProps =(state) => createStructuredSelector({
   currentUser: selectCurrentUser,
-  collectionsArray: selectCollectionForPreview
 });
 
 const mapDispatchToProps = (dispatch) => ({
