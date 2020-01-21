18247334a000b8edeefc0fbc6454cb06ae2729a4
Christian Stevens <cstevens@911MEMORIAL.ORG>
2020-01-21 17:30:22 -0500

    added HOC to shop page to implement loading feature for CollectionPage and CollectionOverview

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -9,25 +9,43 @@ import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/fireb
 
 import { updateCollections }  from '../../redux/shop/shop.actions';
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends Component {
+    state = {
+        loading: true
+    };
+
     unsubscribeFromSnapshot = null;
 
     componentDidMount () {
         const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
         
-        collectionRef.onSnapshot(async snapshot => {
+        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
+            this.setState({ loading: false });
         }) 
     }
     render() {
         const { match } = this.props;
+        const { loading } = this.state;
         return (
             <div className='shop-page'>
-                <Route exact path={`${match.path}`} component={CollectionsOverview} />
-                <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+                <Route 
+                    exact path={`${match.path}`} 
+                    render={(props) => (
+                        <CollectionsOverviewWithSpinner isLoading={ loading } {...props} />)}
+                />
+                <Route 
+                    path={`${match.path}/:collectionId`} 
+                    render={(props) => (
+                        <CollectionPageWithSpinner isLoading={ loading } {...props} />)}
+                />
             </div>
         )
     }
