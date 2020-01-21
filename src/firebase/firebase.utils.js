fab95a93b3719976fb1402008e70a49eaca43516
Christian Stevens <cstevens@911MEMORIAL.ORG>
2020-01-21 15:48:53 -0500

    fixed custom button issue with component-items and converted shopData to redux process

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,8 @@ const config = {
         measurementId: "G-1XJ8YDHX4C"
 };
 
+
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
         if (!userAuth) return;
 
@@ -40,10 +42,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 };
 
 export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
-        console.log('collectionKey',collectionKey);
-        console.log('objectsToAdd',objectsToAdd);
         const collectionRef = firestore.collection(collectionKey);
-        console.log('collectionRef',collectionRef);
         const batch = firestore.batch();
         objectsToAdd.forEach(obj => {
                 const newDocRef = collectionRef.doc();
@@ -53,6 +52,21 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
         return await batch.commit();
 };
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+                const transformedCollection = collections.docs.map(doc => {
+                        const { title, items } = doc.data();
+                return {
+                        routeName:encodeURI(title.toLowerCase()),
+                        id: doc.id,
+                        title,
+                        items
+                }
+        })
+        return transformedCollection.reduce((accumulator, collection) => {
+                accumulator[collection.title.toLowerCase()] = collection;
+                return accumulator;
+        }, {});
+}
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
