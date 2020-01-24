02ad799aeeca70bc87f233d46d9d06749429f289
Christian Stevens <stev1905@gmail.com>
2020-01-24 08:18:23 -0500

    added user-sagas for Google sign in and email signin to handle user Auth functionality

--- src/App.js
+++ src/App.js
@@ -11,31 +11,32 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component.jsx';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends Component {
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+ 
     
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    //   if(userAuth) {
+    //     const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
-            setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-            });
-        });
-      }
+    //     userRef.onSnapshot(snapShot => {
+    //         setCurrentUser({
+    //           id: snapShot.id,
+    //           ...snapShot.data()
+    //         });
+    //     });
+    //   }
 
-      setCurrentUser(userAuth);
-    })
+    //   setCurrentUser(userAuth);
+    //});
   }
+
   componentWillUnmount(props) {
     this.unsubscribeFromAuth(props);
   }
@@ -69,8 +70,5 @@ const mapStateToProps =(state) => createStructuredSelector({
   currentUser: selectCurrentUser,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user))
-});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,19 @@
-import { UserActionTypes } from './user.types'
+import UserActionTypes from './user.types'
 
-export const setCurrentUser = (user) => ({
-    type: UserActionTypes.SET_CURRENT_USER,
+export const emailSignInStart = () => ({
+    type: UserActionTypes.EMAIL_SIGN_IN_START
+});
+
+export const googleSignInStart = () => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_START
+});
+
+export const SignInSuccess = (user) => ({
+    type: UserActionTypes.SIGN_IN_SUCCESS,
     payload: user
-})
+});
+
+export const SignInFailure = (error) => ({
+    type: UserActionTypes.SIGN_IN_FAILURE,
+    payload: error
+});
