72266da11bcd10e4dc140b0da83696768d63b0aa
Christian Stevens <stev1905@gmail.com>
2024-09-23 18:09:17 -0400

    remove nav menu item that has no use

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -20,9 +20,6 @@ const Header = ({ currentUser, hidden, signOutStart }) => (
             <OptionLink to='/shop'>
                 SHOP
             </OptionLink>
-            <OptionLink to='shop'>
-                CONTACT
-            </OptionLink>
             {
                 currentUser ?
                 <OptionLink as='div' onClick={signOutStart}>
