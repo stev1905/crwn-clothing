f70b9b0dd999d523db8e9071731525e6addb15a4
Christian Stevens <stev1905@gmail.com>
2024-09-23 17:55:31 -0400

    refactor update packages

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,7 @@
-import {createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
-
 
 import rootReducers from './root-reducer';
 import rootSaga from './root-saga';
@@ -11,15 +10,21 @@ const sagaMiddleware = createSagaMiddleware();
 
 const middlewares = [sagaMiddleware];
 
-if(process.env.NODE_ENV ==='development') {
+if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
 }
 
+// Create the store
 export const store = createStore(rootReducers, applyMiddleware(...middlewares));
 
+// Run the saga middleware
 sagaMiddleware.run(rootSaga);
 
+// Create the persistor
 export const persistor = persistStore(store);
 
-export default { store, persistor };
+// Assign the store and persistor to a variable
+const reduxStore = { store, persistor };
 
+// Export the variable instead of an anonymous object
+export default reduxStore;
